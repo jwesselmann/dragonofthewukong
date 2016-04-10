bc06cb47133908fcf6a6726e7bc84b6c56d1202c
jwesselmann <james.wesselmann@gmail.com>
2016-04-09 21:34:05 -0500

    Second commit. Added libgdx project.

---
+++ desktop/src/com/baseless/dragonofthewukong/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.baseless.dragonofthewukong.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.baseless.dragonofthewukong.app;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new app(), config);
+	}
+}
---
+++ html/src/com/baseless/dragonofthewukong/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.baseless.dragonofthewukong.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.baseless.dragonofthewukong.app;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new app();
+        }
+}
